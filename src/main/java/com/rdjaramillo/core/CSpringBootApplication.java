992eb76c2dff60c0112f692c6da245312f561907
renzodamian <renzojaramillocueva@gmail.com>
2019-04-23 22:01:36 -0500

    Initial

---
+++ src/main/java/com/rdjaramillo/core/CSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.rdjaramillo.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CSpringBootApplication.class, args);
+	}
+
+}
